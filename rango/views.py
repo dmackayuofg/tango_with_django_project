aa7809eb1c9a5d829184ec893e624449473f7b94
Drew Mackay <2652958m@student.gla.ac.uk>
2025-03-16 19:13:19 +0000

    Chapter 10 guide fix

--- rango/views.py
+++ rango/views.py
@@ -18,9 +18,9 @@ def get_server_side_cookie(request, cookie, default_val=None):
         val = default_val
     return val
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
@@ -39,10 +39,9 @@ def index(request):
     context_dict['pages'] = page_list
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
-    
+
     response = render(request, 'rango/index.html', context=context_dict)
     return response
-
 
 def about(request):
     if request.session.test_cookie_worked():
